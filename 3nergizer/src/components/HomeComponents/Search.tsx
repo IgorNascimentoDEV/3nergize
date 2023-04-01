ba278c004d387e13b06c4f43e130ed4049e17b10
SrLuc <ruanlucenaramos@gmail.com>
2023-03-31 23:14:06 -0300

    adicionando painel com valor do gasto mensal

---
+++ 3nergizer/src/components/HomeComponents/Search.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from "./styles";
+
+interface SearchProps {}
+
+const Search = ({}: SearchProps) => {
+  return <S.StyledSearch></S.StyledSearch>;
+};
+
+export default Search;
