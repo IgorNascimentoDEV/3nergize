b44f1a5c97ff81b862b5adf9a8791aeecfb18cab
SrLuc <ruanlucenaramos@gmail.com>
2023-04-05 01:41:21 -0300

    removendo as funcoes dentro do arquivo calculator.tsx e colocando dentro do arquivo teste.tsx, essas funcoes estao causando erro na aplicacao impedindo o merge

--- 3nergizer/src/pages/Calculator.tsx
+++ 3nergizer/src/pages/Calculator.tsx
@@ -6,6 +6,7 @@ import NavBottomBar from "../components/NavBottomBar/NavBottomBar";
 interface CalculatorProps {}
 
 const Calculator = ({}: CalculatorProps) => {
+
   return (
     <S.StyledCalculator>
       <form >
@@ -30,37 +31,10 @@ const Calculator = ({}: CalculatorProps) => {
     
     <div>O resultado é: </div>
     </form>
+
       <NavBottomBar />
     </S.StyledCalculator>
   );
 };
 
-// Captura os elementos HTML dos inputs
-const kwInput = document.getElementById('numberInput1');
-const dataInput = document.getElementById('dateInput1');
-const kw2Input = document.getElementById('dateInput2');
-const data2Input = document.getElementById('numberInput2');
-
-// Define as chaves de armazenamento para cada input
-const kwKey = 'kwData';
-const dataKey = 'dateData';
-const kw2Key = 'kw2Data';
-const data2Key = 'date2Data';
-
-// Adiciona um evento de mudança para cada input
-kwInput.addEventListener('change', () => {
-  localStorage.setItem(kwKey, kwInput.value);
-});
-
-dataInput.addEventListener('change', () => {
-  localStorage.setItem(dataKey, dataInput.value);
-});
-
-kw2Input.addEventListener('change', () => {
-  localStorage.setItem(kw2Key, kw2Input.value);
-});
-
-data2Input.addEventListener('change', () => {
-  localStorage.setItem(data2Key, data2Input.value);
-});
 export default Calculator;
