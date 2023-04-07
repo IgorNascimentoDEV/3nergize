8d19ce6e038d046899e5dbe232465fa90b921440
igor <diegoigor2001@gmail.com>
2023-04-06 22:48:24 -0300

    criando script calculadora

--- novoTeste/script.js
+++ novoTeste/script.js
@@ -1,56 +1,42 @@
-const url = "https://apise.way2.com.br/v1/bandeiras?apikey=2163780d87ee4237884c498ece5ea7cc&datainicial=2023-03-01&datafinal=2023-03-31";
+ // Captura os elementos HTML dos inputs
+ const leituraInicialInput = document.getElementById('leituraInicial');
+ const dataInicialInput = document.getElementById('dataInicial');
+ const leituraFinalInput = document.getElementById('leituraFinal');
+ const dataFinalInput = document.getElementById('dataFinal');
 
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    const items = data.items;
-    
-    items.forEach(item => {
-      console.log(item.value);
-    });
-  })
-  .catch(error => console.log(error));
+ // Define as tarifas de energia elétrica
+ const tarifaBase = 0.9;
+ const tarifaConsumo = 0.05;
+ const taxaIluminacao = 0.06;
 
- 
-//  // Captura os elementos HTML dos inputs
-//  const leituraInicialInput = document.getElementById('leituraInicial');
-//  const dataInicialInput = document.getElementById('dataInicial');
-//  const leituraFinalInput = document.getElementById('leituraFinal');
-//  const dataFinalInput = document.getElementById('dataFinal');
+ // Adiciona um evento de mudança para o botão de calcular
+ document.getElementById('btnCalcular').addEventListener('click', () => {
+   // Converte os valores dos inputs para números
+   const leituraInicial = Number(leituraInicialInput.value);
+   const leituraFinal = Number(leituraFinalInput.value);
+   const dataInicial = new Date(dataInicialInput.value);
+   const dataFinal = new Date(dataFinalInput.value);
 
-//  // Define as tarifas de energia elétrica
-//  const tarifaBase = 0.9;
-//  const tarifaConsumo = 0.05;
-//  const taxaIluminacao = 0.06;
+   // Calcula a quantidade de dias entre as datas de leitura
+   const periodoDias = Math.ceil((dataFinal - dataInicial) / (1000 * 60 * 60 * 24));
 
-//  // Adiciona um evento de mudança para o botão de calcular
-//  document.getElementById('btnCalcular').addEventListener('click', () => {
-//    // Converte os valores dos inputs para números
-//    const leituraInicial = Number(leituraInicialInput.value);
-//    const leituraFinal = Number(leituraFinalInput.value);
-//    const dataInicial = new Date(dataInicialInput.value);
-//    const dataFinal = new Date(dataFinalInput.value);
+   // Calcula o consumo de energia em kWh
+   const consumo = leituraFinal - leituraInicial;
 
-//    // Calcula a quantidade de dias entre as datas de leitura
-//    const periodoDias = Math.ceil((dataFinal - dataInicial) / (1000 * 60 * 60 * 24));
+   // Calcula o valor da conta de energia elétrica
+   const valorBase = tarifaBase * periodoDias;
+   const valorConsumo = tarifaConsumo * consumo;
+   const valorIluminacao = taxaIluminacao * (valorBase + valorConsumo);
+   const valorTotal = valorBase + valorConsumo + valorIluminacao;
 
-//    // Calcula o consumo de energia em kWh
-//    const consumo = leituraFinal - leituraInicial;
+   // Salva o resultado no local storage
+   const simulacao = {
+     periodo: `${dataInicial.toLocaleDateString()} a ${dataFinal.toLocaleDateString()}`,
+     consumo: `${consumo} kWh`,
+     valorTotal: `R$ ${valorTotal.toFixed(2)}`
+   };
+   localStorage.setItem('simulacao', JSON.stringify(simulacao));
 
-//    // Calcula o valor da conta de energia elétrica
-//    const valorBase = tarifaBase * periodoDias;
-//    const valorConsumo = tarifaConsumo * consumo;
-//    const valorIluminacao = taxaIluminacao * (valorBase + valorConsumo);
-//    const valorTotal = valorBase + valorConsumo + valorIluminacao;
-
-//    // Salva o resultado no local storage
-//    const simulacao = {
-//      periodo: `${dataInicial.toLocaleDateString()} a ${dataFinal.toLocaleDateString()}`,
-//      consumo: `${consumo} kWh`,
-//      valorTotal: `R$ ${valorTotal.toFixed(2)}`
-//    };
-//    localStorage.setItem('simulacao', JSON.stringify(simulacao));
-
-//    // Exibe o resultado para o usuário
-//    document.getElementById('resultado').textContent = `Período: ${simulacao.periodo}\nConsumo: ${simulacao.consumo}\nValor Total: ${simulacao.valorTotal}`;
-//  });
+   // Exibe o resultado para o usuário
+   document.getElementById('resultado').textContent = `Período: ${simulacao.periodo}\nConsumo: ${simulacao.consumo}\nValor Total: ${simulacao.valorTotal}`;
+ });
