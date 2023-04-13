010b0b8669b666d377135ef65cd99f56826d2cfd
Ruan Lucena <100430135+SrLuc@users.noreply.github.com>
2023-04-13 14:03:03 -0300

    add navegation
    adicionando navegação ao botao de consultar na pagina home

--- 3nergizer/src/components/analysisComponents/AnalisyItem.tsx
+++ 3nergizer/src/components/analysisComponents/AnalisyItem.tsx
@@ -3,39 +3,37 @@ import * as S from './styles'
 import { CSSProperties } from 'styled-components';
 
 interface AnalisyPanelItemProps {
-    value?: number;
-    name?: string;
-    img?: string;
-    rate?: number;
-    headerColor?:string;
-    botttomColor?:string;
-  }
-  
-  const AnalisyPanelItem = ({
-    value,
-    name,
-    img,
-    rate,
-    headerColor,
-    botttomColor
-  }: AnalisyPanelItemProps) => {
-    return (
-      <S.StyledAnalisyPanelItem>
-        <header style={{backgroundColor:headerColor}}>
-          <p>R${value}</p>
-        </header>
-  
-        <article>
-          <p>{name}</p>
-          <img src={img} alt="" />
-        </article>
-  
-        <footer style={{backgroundColor:botttomColor}}>
-          <p>{rate}%</p>
-        </footer>
-      </S.StyledAnalisyPanelItem>
-    );
-  };
+  value?: number;
+  name?: string;
+  img?: string;
+  rate?: number;
+  Color?: string;
+}
+
+const AnalisyPanelItem = ({
+  value,
+  name,
+  img,
+  rate,
+  Color
+}: AnalisyPanelItemProps) => {
+  return (
+    <S.StyledAnalisyPanelItem>
+      <header style={{ backgroundColor: Color }}>
+        <p>R${value}</p>
+      </header>
+
+      <article>
+        <p>{name}</p>
+        <img src={img} alt="" />
+      </article>
+
+      <footer style={{ backgroundColor: Color }}>
+        <p>{rate}%</p>
+      </footer>
+    </S.StyledAnalisyPanelItem>
+  );
+};
 
 
 export default AnalisyPanelItem
